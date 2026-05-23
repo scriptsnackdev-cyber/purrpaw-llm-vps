19f612e52d607b808b357ef81ffca5ef244d7796
ScriptSnackDev <script.snack.dev@gmail.com>
2026-05-23 15:37:56 +0700

    tt

--- server.ts
+++ server.ts
@@ -18,7 +18,12 @@ const app = express();
 
 // Custom CORS and Private Network Access (PNA) Middleware
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    const origin = req.headers.origin;
+    if (origin) {
+        res.header("Access-Control-Allow-Origin", origin);
+    } else {
+        res.header("Access-Control-Allow-Origin", "*");
+    }
     res.header("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, Origin, X-Requested-With");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     res.header("Access-Control-Allow-Credentials", "true");
